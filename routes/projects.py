aee010e8217e8cf5cfdcfdd899a7b56d21e49e32
MILIND KUNDU <145224084+smk1808@users.noreply.github.com>
2026-09-09 18:41:51 +0530

    Reformat code in projects.py for consistency

--- routes/projects.py
+++ routes/projects.py
@@ -5,7 +5,6 @@ from services.project_analyzer import analyze_project_zip
 
 router = APIRouter(tags=["Project Analysis"])
 
-
 @router.post("/analyze-project")
 async def analyze_project(file: UploadFile = File(...)):
     with tempfile.NamedTemporaryFile(delete=False) as tmp:
