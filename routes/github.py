7ba2d8758252c10818d645c8a8e5831879405357
MILIND KUNDU <145224084+smk1808@users.noreply.github.com>
2026-09-09 18:38:56 +0530

    Update github.py

--- routes/github.py
+++ routes/github.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter
 from services.github_analyzer import analyze_github_repo
 
 router = APIRouter(tags=["GitHub Analysis"])
-
 @router.post("/analyze-github")
 
 def analyze_github(data: dict):
